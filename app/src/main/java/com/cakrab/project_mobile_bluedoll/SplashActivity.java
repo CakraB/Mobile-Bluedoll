6b047dda8b7cc5d4e3846506640470f8affb999a
CakraB <cakrabuana7255@gmail.com>
2021-12-19 16:31:35 +0700

    Adding Splash Screen

---
+++ app/src/main/java/com/cakrab/project_mobile_bluedoll/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.cakrab.project_mobile_bluedoll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        // Set Delayed Screen before do intent
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, 2000);
+    }
+}
