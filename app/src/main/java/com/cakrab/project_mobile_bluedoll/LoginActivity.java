2f807d5f2149a76c85f3c1749f3c16bf18a97a93
CakraB <cakrabuana7255@gmail.com>
2021-12-20 10:28:11 +0700

    Adding Logic to Login and Register

--- app/src/main/java/com/cakrab/project_mobile_bluedoll/LoginActivity.java
+++ app/src/main/java/com/cakrab/project_mobile_bluedoll/LoginActivity.java
@@ -2,13 +2,79 @@ package com.cakrab.project_mobile_bluedoll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editEmail, editPassword;
+    Button buttonLogin;
+    TextView textRegisterHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
+        setAction();
+    }
+
+    private void init() {
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
+        buttonLogin = findViewById(R.id.button_login);
+        textRegisterHere = findViewById(R.id.text_register_here);
+    }
+
+    private void setAction() {
+        //  Button Login Action
+        buttonLogin.setOnClickListener(view -> {
+            String getEmail = editEmail.getText().toString();
+            String getPassword = editPassword.getText().toString();
+            // Validate Email Input
+            try {
+                if (getEmail.trim().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Email must be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+//                    else if (!users.contains(getEmail)) {
+//                        Toast.makeText(LoginActivity.this, "Your Email is not Registered", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(LoginActivity.this, "Error Found!, Please Try Again", Toast.LENGTH_SHORT).show();
+            }
+            // Validate Password Input
+            try {
+                if (getPassword.trim().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Password must be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+//                    else if (!users.contains(getPassword)) {
+//                        Toast.makeText(LoginActivity.this, "Your Password is Incorrect", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(LoginActivity.this, "Error Found!, Please Try Again", Toast.LENGTH_SHORT).show();
+            }
+            // Validate Success and Redirect to Home
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+            // Reset All Data on Input Fields
+            editEmail.setText("");
+            editPassword.setText("");
+        });
+        // Button Move to Register Page Action
+        textRegisterHere.setOnClickListener(view -> {
+            Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(i);
+        });
     }
 }
--- app/src/main/java/com/cakrab/project_mobile_bluedoll/RegisterActivity.java
+++ app/src/main/java/com/cakrab/project_mobile_bluedoll/RegisterActivity.java
@@ -1,14 +1,151 @@
 package com.cakrab.project_mobile_bluedoll;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText editName, editEmail, editPassword, editPasswordConfirm;
+    RadioGroup radioGender;
+    CheckBox checkTerms;
+    Button buttonRegister;
+    TextView textLoginHere;
+//    EditText editBirthday = findViewById(R.id.edit_birthday);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+        setAction();
+    }
+
+    private void init() {
+        editName = findViewById(R.id.edit_name);
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
+        editPasswordConfirm = findViewById(R.id.edit_password_confirm);
+        radioGender = findViewById(R.id.radio_gender);
+        checkTerms = findViewById(R.id.check_terms);
+        buttonRegister = findViewById(R.id.button_register);
+        textLoginHere = findViewById(R.id.text_login_here);
+//        EditText editBirthday = findViewById(R.id.edit_birthday);
+    }
+
+    private void setAction() {
+        // Button Register Action
+        buttonRegister.setOnClickListener(view -> {
+            String getName = editName.getText().toString();
+            String getEmail = editEmail.getText().toString();
+            String getPassword = editPassword.getText().toString();
+            String getPasswordConfirm = editPasswordConfirm.getText().toString();
+            int selectedGender = radioGender.getCheckedRadioButtonId();
+            // Validate Name Input
+            try {
+                if (getName.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Name must be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(RegisterActivity.this, "Error Found!, Please Try Again", Toast.LENGTH_SHORT).show();
+            }
+            // Validate Email Input
+            try {
+                if (getEmail.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Email must be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (!getEmail.contains("@") || !getEmail.endsWith(".com")) {
+                    Toast.makeText(RegisterActivity.this, "Email is invalid format", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+//                    else if (users.contains(getEmail)) {
+//                        Toast.makeText(RegisterActivity.this, "Email is already exist", Toast.LENGTH_SHORT).show();
+//                        return;
+//                    }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(RegisterActivity.this, "Error Found!, Please Try Again", Toast.LENGTH_SHORT).show();
+            }
+            // Validate Password Input
+            try {
+                if (getPassword.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Password must be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (getPassword.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "Password must be contains at least 6 characters", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(RegisterActivity.this, "Error Found!, Please Try Again", Toast.LENGTH_SHORT).show();
+            }
+            // Validate Password Confirmation Input
+            try {
+                if (getPasswordConfirm.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Password Confirmation must be filled", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (!getPasswordConfirm.equals(getPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Password Confirmation must be match with password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(RegisterActivity.this, "Error Found!, Please Try Again", Toast.LENGTH_SHORT).show();
+            }
+            // Validate Gender Radio Value
+            if (selectedGender == -1) {
+                Toast.makeText(RegisterActivity.this, "Gender must be selected", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            RadioButton radioButton = findViewById(selectedGender);
+            // Validate Terms & Condition Checkbox
+            if (!checkTerms.isChecked()) {
+                Toast.makeText(RegisterActivity.this, "User must agreed on the terms and conditions", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            // Validate Success and Redirect to Login Activity
+//                users.add(getEmail);
+            login(getEmail, getPassword);
+            // Reset All Data on Input Fields
+            editName.setText("");
+            editEmail.setText("");
+            editPassword.setText("");
+            editPasswordConfirm.setText("");
+            radioButton.setChecked(false);
+            checkTerms.setChecked(false);
+        });
+        // Button Move to Login Page Action
+        textLoginHere.setOnClickListener(view -> {
+            Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(i);
+        });
+    }
+
+    // Move with register data to login page
+    public void login(String getEmail, String getPassword) {
+        Random rand = new Random();
+        int keyOne = rand.nextInt(10);
+        int keyTwo = rand.nextInt(10);
+        int keyThree = rand.nextInt(10);
+        String userId = "US" + keyOne + keyTwo + keyThree;
+
+        Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+        i.putExtra("USERID", userId);
+        i.putExtra("EMAIL", getEmail);
+        i.putExtra("PASSWORD", getPassword);
+        startActivity(i);
     }
 }
